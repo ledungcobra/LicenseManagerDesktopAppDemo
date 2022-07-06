dae15e9df9111522b3b0a640b797c11f37febc7a
ledungcobra <ledungcobra@gmail.com>
2022-07-06 21:30:28 +0700

    Update src code

--- src/renderer/utils/Constants.ts
+++ src/renderer/utils/Constants.ts
@@ -1,5 +1,5 @@
 export const RPC_HOST = 'http://127.0.0.1:8545';
-export const APP_ADDRESS = '0x8eDc8DBaa898e065FB6cba7e90DBb9283242BC8F';
+export const APP_ADDRESS = '0xc0B25edE43C152ACD4A3605c3A94a0d088A77f5a';
 export const NETWORK_ID = 5777;
 export const SECRET = 'HEllo world';
 export const TIME_OUT_CHECK_LICENSE = 3000;
